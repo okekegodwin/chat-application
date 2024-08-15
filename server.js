20f1129981f28645ad9eb972fae2f601008b133a
okekegodwin <okekegodwinobiora@gmail.com>
2024-08-15 03:06:13 +0100

    modified this file

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const { createServer } = require("node:http");
 const ejs = require("ejs");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
-const port = process.env.PORT;
+const io = new Server(server);
+const port = process.env.PORT || 8000;
 
 app.set("views", "views");
 app.set("view engine", "ejs");
@@ -14,6 +16,26 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
     res.status(200);
     res.render("index");
+})
+
+io.on("connection", (socket) => {
+    console.log("a user connected", socket.id);
+
+    socket.on("chat message", (msg) => {
+        socket.broadcast.emit("chat message", msg);
+    })
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+    })
+
+    // sending message to the client
+    // socket.emit("message", "Hello from Server");
+
+    // recieving message from the client
+    // socket.on("message", (msg) => {
+    //     console.log("client message:" + " " + msg)
+    // })
 })
 
 server.listen(port, () => {
